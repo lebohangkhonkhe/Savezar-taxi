de87b5dd0cc230c5140cc8fa9b6b3fc818ecbedd
Lebohang Khonkhe <lebohang.khonkhe@gmail.com>
2026-02-07 22:26:24 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,23 +1,21 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.savezar.taxi',
-  appName: 'SaveZar',
-  webDir: 'dist/public',
-  server: {
-    androidScheme: 'https'
+{
+  "appId": "com.savezar.taxi",
+  "appName": "SaveZar",
+  "webDir": "dist/public",
+  "server": {
+    "androidScheme": "https",
+    "url": "https://savezar.com",
+    "cleartext": false
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#DC2626',
-      androidSplashResourceName: 'splash',
-      showSpinner: false
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 2000,
+      "backgroundColor": "#DC2626",
+      "androidSplashResourceName": "splash",
+      "showSpinner": false
     }
   },
-  android: {
-    backgroundColor: '#DC2626'
+  "android": {
+    "backgroundColor": "#DC2626"
   }
-};
-
-export default config;
+}
